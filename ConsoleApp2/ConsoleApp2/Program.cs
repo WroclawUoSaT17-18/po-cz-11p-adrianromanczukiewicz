a101b8be3e17c6e3b13d1600cd51d83b081d1756
adrianromanczukiewicz <36730036+adrianromanczukiewicz@users.noreply.github.com>
2018-03-08 13:15:00 +0100

    8.03

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dziennik
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("podac 3 oceny z danych przedmiotów, nastepnie zatwierdzic kazda klawiszem enter");
+            Console.WriteLine("Polski:");
+            float p = float.Parse(Console.ReadLine());
+            Console.WriteLine("Matematyka:");
+            float o = float.Parse(Console.ReadLine());
+            Console.WriteLine("Wychowanie do życia w rodzinie:");
+            float i = float.Parse(Console.ReadLine());
+            uczen xxx = new uczen(p, o, i);
+            Console.WriteLine(xxx.wartoscsrednia());
+            Console.ReadKey();
+        }
+    }
+    class uczen
+    {
+        public float a;
+        public float s;
+        public float d;
+
+        public uczen(float stopien1, float stopien2, float stopien3)
+        {
+            a = stopien1;
+            s = stopien2;
+            d = stopien3;
+        }
+        public float wartoscsrednia()
+        {
+            Console.WriteLine("średni ocen:");
+            return ((a + s + d) / 3);
+        }
+    }
+}
